7db9100ca192f0a9df1c2686221995defc680283
rrq-dev <raihanhermawan85@gmail.com>
2025-01-21 22:40:29 +0700

    update navigasi windows

--- auth/login.js
+++ auth/login.js
@@ -25,7 +25,8 @@ document
         localStorage.setItem("jwtToken", result.token); // Simpan token ke local storage
         localStorage.setItem("userId", result.user.id); // Simpan user ID yang benar
         localStorage.setItem("userRole", result.user.role); // Simpan role pengguna
-        window.location.href = "index.html"; // Redirect ke halaman beranda
+        window.location.href =
+          "https://rrq-dev.github.io/jumatberkah.github.io/"; // Redirect ke halaman beranda
       } else {
         alert(result.message || "Login failed. Please try again."); // Tampilkan pesan kesalahan
       }
